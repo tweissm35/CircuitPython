71c2c64ae88b73ddcc29a16ac74b772a62be37a0
Timothy Weissman <tweissm35@ccs.local>
2019-10-03 09:57:18 -0400

    git commit -m "finished fancy led"

--- fancyLED.py
+++ fancyLED.py
@@ -1,4 +1,5 @@
 import digitalio #pylint: disable-msg=import-error
+import random
 import time
 
 class fancyLED:
@@ -11,25 +12,53 @@ class fancyLED:
         self.btwo.direction = digitalio.Direction.OUTPUT
         self.bthree.direction = digitalio.Direction.OUTPUT
     def alternate(self):
-        self.bthree = True
-        self.bone = True
+        self.btwo.value = False
+        self.bthree.value = True
+        self.bone.value = True
         time.sleep(1)
-        self.bthree = False
-        self.bone = False
-        self.btwo = True
+        self.btwo.value = True
+        self.bthree.value = False
+        self.bone.value = False
     def off(self):
-        self.bthree = False
-        self.bone = False
-        self.btwo = False
+        self.bthree.value = False
+        self.bone.value = False
+        self.btwo.value = False
     def blink(self):
-        self.bthree = True
-        self.bone = True
-        self.btwo = True
+        self.bthree.value = True
+        self.bone.value = True
+        self.btwo.value = True
         time.sleep(1)
-        self.bthree = False
-        self.bone = False
-        self.btwo = False
+        self.bthree.value = False
+        self.bone.value = False
+        self.btwo.value = False
     def chase(self):
-        #stuff goes here
-        self.bthree = False
+        self.bthree.value = False
+        self.btwo.value = False
+        self.bone.value = True
+        time.sleep(0.5)
+        self.bthree.value = False
+        self.btwo.value = True
+        self.bone.value = False
+        time.sleep(0.5)
+        self.bthree.value = True
+        self.btwo.value = False
+        self.bone.value = False
+        time.sleep(0.5)
+        self.bthree.value = False
+        self.btwo.value = False
+        self.bone.value = False
+    def sparkle(self):
+        ran = random.randint(0,2)
+        if(ran == 0):
+            self.bthree.value = True
+            time.sleep(random.random())
+            self.bthree.value = True
+        elif(ran == 1):
+            self.btwo.value = True
+            time.sleep(random.random())
+            self.btwo.value = False
+        elif(ran == 2):
+            self.bone.value = True
+            time.sleep(random.random())
+            self.bone.value = False
 
--- two_fancy.py
+++ two_fancy.py
@@ -1,13 +1,30 @@
 from fancyLED import fancyLED
+import board #pylint: disable-msg=import-error
+import time
+f1 = board.D13
+f2 = board.D12
+f3 = board.D11
 
-fancy1 = fancyLED(13,12,11)
-fancy2 = fancyLED(5,6,7)
+b1 = board.D10
+b2 = board.D9
+b3 = board.D8
 
-while True:
-    #fancy1.alternate()
-    fancy1.blink()
-    fancy1.blink()
-    fancy1.blink()
-    fancy1.blink()
-   # fancy1.chase()
-    #fancy2.sparkle()
+fancy1 = fancyLED(f1,f2,f3)
+fancy2 = fancyLED(b1,b2,b3)
+
+#while True:
+fancy1.alternate()
+fancy2.alternate()
+time.sleep(1)
+fancy1.off()
+fancy2.off()
+time.sleep(1)
+fancy1.blink()
+fancy2.blink()
+time.sleep(1)
+fancy1.off()
+fancy2.off()
+time.sleep(1)
+fancy1.chase()
+fancy2.chase()
+#fancy2.sparkle()
