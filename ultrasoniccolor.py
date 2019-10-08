9d08c8cce45887d4237270b5a0d097c732ff9964
Timothy Weissman <tweissm35@ccs.local>
2019-10-08 09:30:51 -0400

    git add ultrasoniccolor.py

--- ultrasoniccolor.py
+++ ultrasoniccolor.py
@@ -11,7 +11,9 @@ sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D4, echo_pin=board.D3)#create a
 
 while True:
     try:#do this unless there is an error
-        color = fancy.CHSV(int(round(mapp(clamp(sonar.distance,5,35), 5, 35, 255, 85))), 255, 50)#take the distance value and make the minimum 5cm and the max 35cm, then map the distance value in its range to a range from 255 to 85, then set the hue component of "color" equal to it. Saturation is permenantly 255 and value is always 50
+        color = fancy.CHSV(int(round(mapp(clamp(sonar.distance,5,35), 5, 35, 255, 85))), 255, 50)
+        #↑↑↑ take the distance value and make the minimum 5cm and the max 35cm, then map the distance value in its range to a range from 255 to 85,
+        #↑↑↑ then set the hue component of "color" equal to it. Saturation is permenantly 255 and value is always 50
         packed = color.pack()#turn hue, saturation, and value into one number called "packed"
         dot.fill(packed)#make the led turn whatever color packed is
     except RuntimeError:#if there is an error do this (with the ultrasonic sensor this usually means something is too far away)
