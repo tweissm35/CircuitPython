e738648c16833f82cf7cf4fdb2d05cf5fdacbb0e
Timothy Weissman <tweissm35@ccs.local>
2019-10-15 09:14:26 -0400

    added diagram for fade

--- fade.py
+++ fade.py
@@ -1,11 +1,8 @@
 import board #pylint: disable-msg=import-error
-import neopixel #pylint: disable-msg=import-error
 import time
 import pulseio #pylint: disable-msg=import-error
 
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 led = pulseio.PWMOut(board.D12, frequency=5000, duty_cycle=65535)#PWM output
-pwm = pulseio.PWMOut(board.D13, frequency=50, duty_cycle=0)
 while True:
     for i in range(100):#i increments from 0 to 100
         led.duty_cycle = int(i / 100 * 65535)#sets duty cycle to a percentage of range
